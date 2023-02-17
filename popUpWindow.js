bbe9ca5f51e80adf1b9fbf5ec689a642db3c0967
Kakegurui-Mary <125656150+Kakegurui-Mary@users.noreply.github.com>
2023-02-17 13:50:01 +0100

    Add files via upload

---
+++ popUpWindow.js
@@ -0,0 +1,34 @@
+// Get the button that opens the pop-up window
+var createPostBtn = document.getElementById("create-post-btn");
+
+// Get the pop-up window
+var popup = document.getElementById("create-post-popup");
+
+// Get the close button
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the pop-up window
+createPostBtn.onclick = function() {
+  popup.style.display = "block";
+}
+
+// When the user clicks on the close button, close the pop-up window
+closeBtn.onclick = function() {
+  popup.style.display = "none";
+}
+
+// When the user clicks outside of the pop-up window, close it
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
